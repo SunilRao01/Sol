3f482a3b343f03c0ce5bdbb7d75ce4f8fc784854
cortega2 <corteg20@gmail.com>
2015-05-15 05:42:51 -0500

    Audio stuff

---
+++ Sol/Assets/Scripts/Audio/AudioSpectrum.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSpectrum : MonoBehaviour {
+    AudioSource audio;
+    //test light
+    public Light lowLight;
+    public Light midLight;
+    public Light hiLight;
+
+    private FrequencySegment lowFreq = new FrequencySegment();
+    private FrequencySegment midFreq = new FrequencySegment();
+    private FrequencySegment hiFreq = new FrequencySegment();
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float[] spectrum = new float[1024];
+        audio.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+        //sampling rate = 24000 
+
+        //0 - 150 hz
+        float lowVal = getSum(0, 5, spectrum);
+        
+        //300 - ~420hz
+        float midVal = getSum(13, 18, spectrum);
+        //1000 - 1123
+        float HiVal = getSum(43, 48, spectrum);
+
+        bool lowResult = lowFreq.addSample(lowVal);
+        bool midResult = midFreq.addSample(midVal);
+        bool hiResult = hiFreq.addSample(HiVal);
+
+        //make check
+        if(!lowFreq.filling){
+            if(lowResult){
+                lowLight.intensity = 8;
+             }
+            else {
+                lowLight.intensity = 0;
+            }
+        }
+
+        if (!midFreq.filling) {
+            if (midResult) {            
+                midLight.intensity = 8;
+            }
+            else {
+                midLight.intensity = 0;
+            }
+        }
+
+        if (!hiFreq.filling) {
+            if (hiResult) {
+                hiLight.intensity = 8;
+            }
+            else {
+                hiLight.intensity = 0;
+            }
+        }
+
+        // sample demo stuff used for reference, will delete later
+        int i = 1;
+        float max = 0.0f;
+        int maxIndex = 0;
+        while (i < 1023) {
+            if(spectrum[i] > max){
+                max = spectrum[i];
+                maxIndex = i;
+            }
+            Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.yellow);
+            i++;
+        }
+	}
+
+    private float getSum(int lo, int hi, float[] samples) {
+        float sum = 0.0f;
+        for (int i = lo; i <= hi; i++) {
+            sum += samples[i];
+        }
+
+        return sum;
+    }
+}
---
+++ Sol/Assets/Scripts/Audio/FrequencySegment.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+
+public class FrequencySegment  {
+    private const int SAMPLE_SIZE = 60;
+
+    private  float[] samples = new float[SAMPLE_SIZE];
+    private float average = 0.0f;
+    private float sum = 0.0f;
+    private float minVal = 3.0f;    //might not be used
+    private float maxVal = -3.0f;   //might not be used
+    private int sCounter = 0;
+    
+    public bool filling = true;
+    public float curVal = 0.0f;
+
+    //returns true if significant change
+    public bool addSample(float val) {
+        curVal = val;
+
+        if (sCounter >= SAMPLE_SIZE) {
+            filling = false;
+            sCounter = 0;
+        }
+
+        //update sum
+        if (filling) {
+            sum += curVal;
+        }
+        else {
+            sum -= samples[sCounter];
+            sum += curVal;
+        }
+
+        //update average
+        average = sum / SAMPLE_SIZE;
+
+        //fill buffer
+        samples[sCounter] = curVal;
+
+        //update min, max
+        if (samples[sCounter] > maxVal) {
+            maxVal = samples[sCounter];
+        }
+        if (minVal < samples[sCounter]) {
+            minVal = samples[sCounter];
+        }
+
+        //update counter
+        sCounter++;
+
+        //make check
+        if (!filling) {
+            if (curVal > average) {
+                if (curVal > average + (CalculateStdDev() * 1.5)) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        //Debug.Log("Cur: " + curVal + " AVG: " + average);
+        return false;
+    }
+
+    private float CalculateStdDev() {
+        float ret = 0.0f;
+       
+        //Perform the Sum of (value-avg)_2_2
+        float sum = 0.0f;
+        for(int i = 0; i< SAMPLE_SIZE; i++){
+            sum += (float)(Math.Pow(average - samples[i], 2));
+        }
+
+        //Put it all together      
+        ret = (float)(Math.Sqrt((sum) / (SAMPLE_SIZE)));
+
+        return ret;
+    }
+
+}
